5c21426fe818b7d4131ae0c85c91b415e0131ef4
Allen Porter <allen.porter@gmail.com>
2023-02-05 20:05:16 -0800

    Allow building resources without a Kustomization (#20)
    Allow building with `kustomize cfg grep` without needing to first build
    from a `Kustomization`. Includes a breaking change to move `build` out
    of `Kustomization` into the module level.

--- tests/test_kustomize.py
+++ tests/test_kustomize.py
@@ -4,32 +4,39 @@ from pathlib import Path
 
 import pytest
 
-from flux_local import command
-from flux_local.kustomize import Kustomize
+from flux_local import command, kustomize
 
 TESTDATA_DIR = Path("tests/testdata")
 
 
 async def test_build() -> None:
     """Test a kustomize build command."""
-    result = await Kustomize.build(TESTDATA_DIR / "repo").run()
+    result = await kustomize.build(TESTDATA_DIR / "repo").run()
     assert "Secret" in result
     assert "ConfigMap" in result
     assert result == (TESTDATA_DIR / "repo/all.golden").read_text()
 
 
-async def test_grep() -> None:
-    """Test a kustomize build command."""
-    result = await Kustomize.build(TESTDATA_DIR / "repo").grep("kind=ConfigMap").run()
+async def test_build_grep() -> None:
+    """Test a kustomize build and grep command chained."""
+    result = await kustomize.build(TESTDATA_DIR / "repo").grep("kind=ConfigMap").run()
     assert "Secret" not in result
     assert "ConfigMap" in result
-    assert result == (TESTDATA_DIR / "repo/configmap.golden").read_text()
+    assert result == (TESTDATA_DIR / "repo/configmap.build.golden").read_text()
+
+
+async def test_grep() -> None:
+    """Test a kustomize grep command."""
+    result = await kustomize.grep("kind=ConfigMap", TESTDATA_DIR / "repo").run()
+    assert "Secret" not in result
+    assert "ConfigMap" in result
+    assert result == (TESTDATA_DIR / "repo/configmap.grep.golden").read_text()
 
 
 async def test_objects() -> None:
     """Test loading yaml documents."""
-    kustomize = Kustomize.build(TESTDATA_DIR / "repo").grep("kind=ConfigMap")
-    result = await kustomize.objects()
+    cmd = kustomize.build(TESTDATA_DIR / "repo").grep("kind=ConfigMap")
+    result = await cmd.objects()
     assert len(result) == 1
     assert result[0].get("kind") == "ConfigMap"
     assert result[0].get("apiVersion") == "v1"
@@ -37,14 +44,14 @@ async def test_objects() -> None:
 
 async def test_validate_pass() -> None:
     """Test applying policies to validate resources."""
-    kustomize = Kustomize.build(TESTDATA_DIR / "repo")
-    await kustomize.validate(TESTDATA_DIR / "policies/pass.yaml")
+    cmd = kustomize.build(TESTDATA_DIR / "repo")
+    await cmd.validate(TESTDATA_DIR / "policies/pass.yaml")
 
 
 async def test_validate_fail() -> None:
     """Test applying policies to validate resources."""
-    kustomize = Kustomize.build(TESTDATA_DIR / "repo")
+    cmd = kustomize.build(TESTDATA_DIR / "repo")
     with pytest.raises(
         command.CommandException, match="require-test-annotation: validation error"
     ):
-        await kustomize.validate(TESTDATA_DIR / "policies/fail.yaml")
+        await cmd.validate(TESTDATA_DIR / "policies/fail.yaml")
