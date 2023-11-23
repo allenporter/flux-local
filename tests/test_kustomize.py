b6c7a816df2a33fa84e1d99f1aeba1e2f1d36e2b
Allen Porter <allen@thebends.org>
2023-11-23 05:01:56 +0000

    Remove dead kustomize code

--- tests/test_kustomize.py
+++ tests/test_kustomize.py
@@ -21,30 +21,6 @@ resources:
     "path",
     [TESTDATA_DIR / "repo", (TESTDATA_DIR / "repo").absolute()],
 )
-async def test_build(path: Path, snapshot: SnapshotAssertion) -> None:
-    """Test a kustomize build command."""
-    result = await kustomize.build(path).run()
-    assert "Secret" in result
-    assert "ConfigMap" in result
-    assert result == snapshot
-
-
-@pytest.mark.parametrize(
-    "path",
-    [TESTDATA_DIR / "repo", (TESTDATA_DIR / "repo").absolute()],
-)
-async def test_build_grep(path: Path, snapshot: SnapshotAssertion) -> None:
-    """Test a kustomize build and grep command chained."""
-    result = await kustomize.build(path).grep("kind=ConfigMap").run()
-    assert "Secret" not in result
-    assert "ConfigMap" in result
-    assert result == snapshot
-
-
-@pytest.mark.parametrize(
-    "path",
-    [TESTDATA_DIR / "repo", (TESTDATA_DIR / "repo").absolute()],
-)
 async def test_grep(path: Path, snapshot: SnapshotAssertion) -> None:
     """Test a kustomize grep command."""
     result = await kustomize.grep("kind=ConfigMap", path).run()
@@ -59,7 +35,7 @@ async def test_grep(path: Path, snapshot: SnapshotAssertion) -> None:
 )
 async def test_objects(path: Path, snapshot: SnapshotAssertion) -> None:
     """Test loading yaml documents."""
-    cmd = kustomize.build(path).grep("kind=ConfigMap")
+    cmd = kustomize.grep("kind=ConfigMap", path)
     result = await cmd.objects()
     assert len(result) == 1
     assert result[0].get("kind") == "ConfigMap"
@@ -73,7 +49,7 @@ async def test_objects(path: Path, snapshot: SnapshotAssertion) -> None:
 )
 async def test_stash(path: Path) -> None:
     """Test loading yaml documents."""
-    cmd = await kustomize.build(path).stash()
+    cmd = await kustomize.grep("kind=Ignored", path, invert=True).stash()
     result = await cmd.grep("kind=ConfigMap").objects()
     assert len(result) == 1
     assert result[0].get("kind") == "ConfigMap"
@@ -92,7 +68,7 @@ async def test_stash(path: Path) -> None:
 )
 async def test_validate_pass(path: Path) -> None:
     """Test applying policies to validate resources."""
-    cmd = kustomize.build(path)
+    cmd = kustomize.grep("kind=ConfigMap", path)
     await cmd.validate(TESTDATA_DIR / "policies/pass.yaml")
 
 
@@ -102,98 +78,16 @@ async def test_validate_pass(path: Path) -> None:
 )
 async def test_validate_fail(path: Path) -> None:
     """Test applying policies to validate resources."""
-    cmd = kustomize.build(path)
+    cmd = kustomize.grep("kind=ConfigMap", path)
     with pytest.raises(
         exceptions.CommandException, match="require-test-annotation: validation error"
     ):
         await cmd.validate(TESTDATA_DIR / "policies/fail.yaml")
 
 
-async def test_cannot_kustomize(tmp_path: Path) -> None:
-    """Test that empty directories cannot be kustomized."""
-    assert not await kustomize.can_kustomize_dir(tmp_path)
-
-
-async def test_can_kustomize(tmp_path: Path) -> None:
-    """Test that empty directories cannot be kustomized."""
-    ks = tmp_path / "kustomization.yaml"
-    ks.write_text(KUSTOMIZATION)
-    assert await kustomize.can_kustomize_dir(tmp_path)
-
-
-async def test_fluxtomize_file(tmp_path: Path) -> None:
-    """Test implicit kustomization of files in a directory."""
-    settings = (TESTDATA_DIR / "repo/cluster-settings.yaml").read_text()
-    example_yaml = tmp_path / "example.yaml"
-    example_yaml.write_text(settings)
-
-    content = await kustomize.fluxtomize(tmp_path)
-    assert content
-    assert content.decode("utf-8").split("\n") == [
-        "---",
-        "apiVersion: v1",
-        "kind: ConfigMap",
-        "metadata:",
-        "  namespace: flux-system",
-        "  name: cluster-settings",
-        "data:",
-        "  CLUSTER: dev",
-        "  DOMAIN: example.org",
-        "",
-    ]
-
-
-async def test_fluxtomize_subdir(tmp_path: Path) -> None:
-    """Test implicit kustomization of subdirectories that can be kustomized."""
-    subdir = tmp_path / "subdir"
-    subdir.mkdir()
-    ks = subdir / "kustomization.yaml"
-    ks.write_text(KUSTOMIZATION)
-
-    settings = (TESTDATA_DIR / "repo/cluster-settings.yaml").read_text()
-    example_yaml = subdir / "example.yaml"
-    example_yaml.write_text(settings)
-
-    content = await kustomize.fluxtomize(tmp_path)
-    assert content
-    assert content.decode("utf-8").split("\n") == [
-        "---",
-        "apiVersion: v1",
-        "data:",
-        "  CLUSTER: dev",
-        "  DOMAIN: example.org",
-        "kind: ConfigMap",
-        "metadata:",
-        "  name: cluster-settings",
-        "  namespace: flux-system",
-        "",
-    ]
-
-
-async def test_fluxtomize_ignores_empty_subdir(tmp_path: Path) -> None:
-    """Test implicit kustomization."""
-    subdir = tmp_path / "subdir"
-    subdir.mkdir()
-
-    content = await kustomize.fluxtomize(tmp_path)
-    assert not content
-
-
-async def test_build_flags(snapshot: SnapshotAssertion) -> None:
-    """Test a kustomize build command with extra flags."""
-    result = await kustomize.build(
-        TESTDATA_DIR / "repo",
-        # Duplicates existing flags, should be a no-op
-        kustomize_flags=["--load-restrictor=LoadRestrictionsNone"],
-    ).run()
-    assert "Secret" in result
-    assert "ConfigMap" in result
-    assert result == snapshot
-
-
 async def test_target_namespace() -> None:
     """Test a kustomization with a target namespace."""
-    ks = kustomize.build(TESTDATA_DIR / "repo").grep("kind=ConfigMap")
+    ks = kustomize.grep("kind=ConfigMap", TESTDATA_DIR / "repo")
 
     result = await ks.objects()
     assert len(result) == 1
@@ -204,7 +98,9 @@ async def test_target_namespace() -> None:
         "namespace": "flux-system",
         "annotations": {
             "config.kubernetes.io/index": "0",
+            "config.kubernetes.io/path": "cluster-settings.yaml",
             "internal.config.kubernetes.io/index": "0",
+            "internal.config.kubernetes.io/path": "cluster-settings.yaml",
         },
     }
 
@@ -218,6 +114,8 @@ async def test_target_namespace() -> None:
         "namespace": "configs",
         "annotations": {
             "config.kubernetes.io/index": "0",
+            "config.kubernetes.io/path": "cluster-settings.yaml",
             "internal.config.kubernetes.io/index": "0",
+            "internal.config.kubernetes.io/path": "cluster-settings.yaml",
         },
     }
