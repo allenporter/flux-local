f09911879821d32f066ca540c694526801a7a18d
allenporter <6026418+allenporter@users.noreply.github.com>
2024-11-30 00:42:52 +0000

    chore: accept new Cruft update

--- script/run-mypy.sh
+++ script/run-mypy.sh
@@ -5,8 +5,11 @@ set -o errexit
 # other common virtualenvs
 my_path=$(git rev-parse --show-toplevel)
 
-if [ -f "${my_path}/venv/bin/activate" ]; then
-  . "${my_path}/venv/bin/activate"
-fi
+for venv in venv .venv .; do
+  if [ -f "${my_path}/${venv}/bin/activate" ]; then
+    . "${my_path}/${venv}/bin/activate"
+    break
+  fi
+done
 
 mypy ${my_path}
