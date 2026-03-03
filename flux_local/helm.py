8004e70ddfaa6085051e50e14b7a92cdce3f001c
zili <neodejack@gmail.com>
2026-03-03 15:51:19 +0800

    feat: add more helm template flags

--- flux_local/helm.py
+++ flux_local/helm.py
@@ -240,6 +240,21 @@ class Options:
     registry_config: str | None = None
     """Value of the helm --registry-config flag."""
 
+    is_upgrade: bool = False
+    """Set .Release.IsUpgrade instead of .Release.IsInstall."""
+
+    include_crds: bool = False
+    """Include CRDs in the helm template output."""
+
+    no_hooks: bool = False
+    """Exclude hook-annotated templates from the output."""
+
+    show_only: list[str] | None = None
+    """Render only specific template files."""
+
+    enable_dns: bool = False
+    """Enable DNS lookups during rendering."""
+
     skip_invalid_paths: bool = False
     """Skip HelmReleases with invalid local paths."""
 
@@ -263,6 +278,17 @@ class Options:
             args.extend(["--kube-version", self.kube_version])
         if self.api_versions:
             args.extend(["--api-versions", self.api_versions])
+        if self.is_upgrade:
+            args.append("--is-upgrade")
+        if self.include_crds:
+            args.append("--include-crds")
+        if self.no_hooks:
+            args.append("--no-hooks")
+        if self.show_only:
+            for template in self.show_only:
+                args.extend(["--show-only", template])
+        if self.enable_dns:
+            args.append("--enable-dns")
         return args
 
     @property
